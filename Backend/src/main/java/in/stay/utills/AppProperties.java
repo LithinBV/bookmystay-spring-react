b66cb8e2237453a353ce1c47c8d41946dae253c4
LithinBV <lithinbv39@gmail.com>
2026-02-26 12:59:49 +0530

    Initial commit - BookMyStay Fullstack Project

---
+++ Backend/src/main/java/in/stay/utills/AppProperties.java
@@ -0,0 +1,8 @@
+package in.stay.utills;
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="dus4cb3wt";
+   public static final String CLOUDINARY_API_KEY="959863745122378";
+   public static final String CLOUDINARY_SECRET_KEY="WsmRVuktqSGuRN-Q525ssyxdbjc";
+}
